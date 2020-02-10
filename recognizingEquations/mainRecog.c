2483fc80b804dbbaa3afb0f28c4a8f8d1813403f
Diego Sariol <sarioldiego@yahoo.com>
2020-02-10 16:56:51 +0100

    Assignment 2 start files

---
+++ recognizingEquations/mainRecog.c
@@ -0,0 +1,11 @@
+#include <stdlib.h> 
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include "scanner.h"
+#include "recognizeExp.h"
+
+int main(int argc, char *argv[]) {
+  recognizeExpressions();
+  return 0;
+}
---
+++ solvingEquations/mainScan.c
@@ -0,0 +1,6 @@
+#include "scanner.h"
+
+int main(int argc, char *argv[]) {
+  scanExpressions();
+  return 0;
+}
