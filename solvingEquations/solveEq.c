001f665c519d46af60558b50801cf4e04fda4b2e
Rares Dobre <rares@RN-145-90-182-174.eduroam.rug.nl>
2020-02-17 00:13:32 +0100

    fixed comments for recognize, added fix for solver

--- solvingEquations/solveEq.c
+++ solvingEquations/solveEq.c
@@ -10,57 +10,63 @@ double solve(List li){
     double x = 0.0;
     int equalsDetected = 0, a=0, b=0;
 // We get an equation in the form ax+b = cx+d, so we restrict it to ex+f=0
-    if(acceptCharacter(&li, '=')) equalsDetected=1;
-    if(acceptIdentifier(&li)){
-        if(equalsDetected==0) a++;
-        else a--;
-    }
-    if(li != NULL && li->tt == Number){
-        int value = li->t.number;
+    while(li != NULL){
+        if(acceptCharacter(&li, '=')) equalsDetected=1;
         if(acceptIdentifier(&li)){
-            if(equalsDetected==0) a += value;
-            else a -= value;
-        } else {
-            if(equalsDetected == 0) b += value;
-            else b -= value;
-            li = li->next;
-        }
-    }
-    if(acceptCharacter(&li, '-')){
-        if(acceptIdentifier(&li)){
-            if(equalsDetected==0) a--;
-            else a++;
-        }
-        if(li != NULL && li->tt == Number){
-            int value = li->t.number;
-            if(acceptIdentifier(&li)){
-                if(equalsDetected==0) a -= value;
-                else a += value;
-            } else {
-                if(equalsDetected == 0) b -= value;
-                else b += value;
-                li = li->next;
+            if(equalsDetected==0) {
+                a++;
+            }
+            else {
+                a--;
             }
         }
-    }
-    if(acceptCharacter(&li, '+')){
-        if(acceptIdentifier(&li)){
-            if(equalsDetected==0) a++;
-            else a--;
-        }
         if(li != NULL && li->tt == Number){
             int value = li->t.number;
+            li = li->next;
             if(acceptIdentifier(&li)){
                 if(equalsDetected==0) a += value;
                 else a -= value;
             } else {
                 if(equalsDetected == 0) b += value;
                 else b -= value;
+            }
+        }
+        if(acceptCharacter(&li, '-')){
+            if(acceptIdentifier(&li)){
+                if(equalsDetected==0) a--;
+                else a++;
+            }
+            if(li != NULL && li->tt == Number){
+                int value = li->t.number;
                 li = li->next;
+                if(acceptIdentifier(&li)){
+                    if(equalsDetected==0) a -= value;
+                    else a += value;
+                } else {
+                    if(equalsDetected == 0) b -= value;
+                    else b += value;
+                }
+            }
+        }
+        if(acceptCharacter(&li, '+')){
+            if(acceptIdentifier(&li)){
+                if(equalsDetected==0) a++;
+                else a--;
+            }
+            if(li != NULL && li->tt == Number){
+                int value = li->t.number;
+                li = li->next;
+                if(acceptIdentifier(&li)){
+                    if(equalsDetected==0) a += value;
+                    else a -= value;
+                } else {
+                    if(equalsDetected == 0) b += value;
+                    else b -= value;
+                }
             }
         }
     }
-    x = (double) -b/a;
+    x = (double)(-b)/a;
     return x;
 }
 
@@ -79,8 +85,16 @@ void solveEquations(){
         if(variableCounter(tl)){
             printf(", but not in 1 variable\n");
         }
-        else printf(" in 1 variable of degree %d\nsolution: %.3lf", degree(tl),
-    solve(tl));
+        else {
+            printf(" in 1 variable of degree %d\n", degree(tl));
+            if(degree(tl) == 1) {
+                double answer = solve(tl);
+                if(answer >= -0.0005 && answer <= 0.0000) {
+                    printf("solution: 0.000\n");
+                }
+                else printf("solution: %.3lf\n", answer);
+            }
+        }
       }
       else {
         printf("this is not an equation\n");
