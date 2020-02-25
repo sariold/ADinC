e50547ad70d928dab5cfa631b2ab44c94001a77a
Diego Sariol <sarioldiego@yahoo.com>
2020-02-25 19:28:01 +0100

    no leaks, updating files

---
+++ spellChecker/speller.c
@@ -0,0 +1,55 @@
+// a SLOW spell checker
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "dictionary.h"
+
+// remove non-alphabetic characters and convert to lower case
+void trimWord(char *word) {
+  int k = 0;
+  for (int i = 0; i < (int) strlen(word); i++) {
+    if (isalpha(word[i])) {
+      word[k] = tolower(word[i]);
+      k++;
+    }
+  }
+  word[k] = '\0';
+}
+
+int main(int argc, char *argv[]) {
+
+  char word[LENGTH + 1] = "";
+
+  // step 1: read in the dictionary
+  trie new = newEmptyTrie();
+  while (scanf("%45s",word) && word[0] != '!') {
+    trimWord(word);
+    addWordTrie(word, new);
+  }
+
+  // step 2: read in text
+  int counter = 0; // number of unknown words
+  int index = 0;
+  int c = getchar();
+  while ((c = getchar()) && c != EOF) {
+      if(isalpha(c)) {
+          word[index] = tolower(c);
+          index++;
+      } else {
+          word[index] = '\0';
+          if(index > 0 && !checkTrie(word, new)) {
+              counter++;
+              printf("%s\n", word);
+          }
+          index = 0;
+      }
+  }
+
+  // step 3: print number of unknown words
+  printf("%d\n", counter);
+
+  freeTrie(new);
+  return 0;
+}
