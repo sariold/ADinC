6b343898748e01ac3db0bffedcdf08a3afac4660
Rares Dobre <rares@192-168-0-103.rdsnet.ro>
2020-03-25 00:21:19 +0200

    commented version

--- 5trains/graphs.c
+++ 5trains/graphs.c
@@ -18,9 +18,10 @@ City ID's:
     Leeuwarden: 10
     Groningen: 11
 In certain functions we subtract 1 from the city id's, because the numbering
-of cities starts from 1, not 0
+of cities starts from 1, not 0.
 */
 
+/*Function that converts strings (city names) to integers*/
 int cityToId(char *s){
     char amsterdam[10] = "Amsterdam";
     char haag[9] = "Den Haag";
@@ -47,6 +48,7 @@ int cityToId(char *s){
     return 0;
 }
 
+/*Function to print the name of a city based on the id*/
 void printCityFromId(int id){
     switch (id) {
         case 1:{
@@ -96,6 +98,7 @@ void printCityFromId(int id){
     }
 }
 
+/*Function to read the input of city names*/
 char *readInput() {
   int strLen = 30;
   int c = getchar();
@@ -123,6 +126,8 @@ void printHeap(Heap hp){
     printf("\n");
 }
 
+/*Function that given a heap and a city id, returns the position in the heap
+array of the city that matches the id */
 int getHeapLocation(Heap hp, int city){
     int location = 0;
     for(int i = 0; i < hp.front; i++){
@@ -131,9 +136,12 @@ int getHeapLocation(Heap hp, int city){
     return location;
 }
 
+/*Function that computes the minimal distance from a starting node to an
+ending node and outputs the shortest path*/
 void dijkstra(List neighbourList[11], int startingNode, int endingNode){
     int path[11];
     int visited[11];
+    /*Initialize the path and visited arrays*/
     for(int i = 0; i < 11; i++){
         path[i] = -1;
         visited[i] = 0;
@@ -141,54 +149,72 @@ void dijkstra(List neighbourList[11], int startingNode, int endingNode){
     heapNode dist[11];
     for(int i = 0; i < 11; i++){
         if(i == startingNode - 1){
+            /*The pseudodistance of the starting node is set to 0*/
             dist[i].pseudodistance = 0;
             dist[i].id = startingNode;
         }
         else {
+            /*The pseudodistance of other nodes is set to 99999*/
             dist[i].pseudodistance = 99999;
             dist[i].id = i + 1;
         }
     }
+    /*Initializing the toDoList with nodes from the dist array. This is used
+    to implement a min-priority queue based on the pseudodistances of nodes*/
     Heap toDoList = makeHeap();
     for(int i = 0; i < 11; i++){
         enqueue(dist[i], &toDoList);
     }
     while(!isEmptyHeap(toDoList)){
         heapNode u = removeMin(&toDoList);
+        /*Removed a node where the initial pseudodistance was not updated,
+        so it is unreachable*/
         if(u.pseudodistance == 99999) break;
+        /*Mark the node as visited*/
         visited[u.id - 1] = 1;
+        /*Ending node found*/
         if(u.id == endingNode){
+            /*Reconstruct the path and output the distance*/
             reconstructPath(path, endingNode, startingNode);
             printf("%d\n", u.pseudodistance);
             free(toDoList.array);
             return;
         }
         List li = neighbourList[u.id - 1];
+        /*Check all the neighbours of the removed node*/
         while(li != NULL){
             int neighbourLocation = getHeapLocation(toDoList, li->node);
             if(visited[li->node - 1] == 0 && (toDoList.array[neighbourLocation]).pseudodistance > u.pseudodistance + li->weight){
+                /*Shorter path found, update the pseudodistance*/
                 (toDoList.array[neighbourLocation]).pseudodistance = u.pseudodistance + li->weight;
                 path[li->node - 1] = u.id;
+                /*Upheap to restore the order*/
                 upheap(&toDoList, neighbourLocation);
             }
+            /*Move to the next neighbour*/
             li = li->next;
         }
     }
+    /*Unreachable node found, output UNREACHABLE and free the heap*/
     printf("UNREACHABLE\n");
     free(toDoList.array);
 }
 
+/*Function that given an ending node and a starting node, it outputs the
+shortest path from the starting node to the ending node*/
 void reconstructPath(int path[11], int node, int start){
     int ar[11], j=0;
     for(int i = 0; i < 11; i++){
         ar[i] = -1;
     }
     while(node != start){
+        /*Backtrack the path and add the id's to the array ar*/
         ar[j] = node;
         j++;
         node = path[node - 1];
     }
     ar[j] = node;
+    /*Print the path*/
     for(int i = 10; i >= 0; i--){
         if(ar[i] != -1){
             printCityFromId(ar[i]);
@@ -196,7 +222,7 @@ void reconstructPath(int path[11], int node, int start){
     }
 }
 
-
+/*Removes the minimum node from the heap*/
 heapNode removeMin(Heap *hp){
     heapNode n;
     if(isEmptyHeap(*hp)){
@@ -204,7 +230,9 @@ heapNode removeMin(Heap *hp){
     }
     n = hp->array[1];
     hp->front--;
+    /*Set the first node to the last one*/
     hp->array[1] = hp->array[hp->front];
+    /*Downheap to restore order*/
     downheap(hp, 1);
     return n;
 }
@@ -215,6 +243,7 @@ void swap(heapNode *a, heapNode *b){
     *b = h;
 }
 
+/*Function to downheap nodes in a heap based on their pseudodistance*/
 void downheap (Heap *hp, int n){
     int fr = hp->front ;
     int indexMax = n ;
@@ -233,6 +262,7 @@ void downheap (Heap *hp, int n){
     }
 }
 
+/*Function to upheap ndoes in a heap based on their pseudodistance*/
 void upheap(Heap *hp, int n){
     if(n > 1 && (hp->array[n]).pseudodistance < (hp->array[n/2]).pseudodistance){
         swap(&hp->array[n], &hp->array[n/2]);
@@ -240,6 +270,7 @@ void upheap(Heap *hp, int n){
     }
 }
 
+/*Doubles the heap size*/
 void doubleHeapSize(Heap *hp){
     int newSize = 2 * hp->size;
     hp->array = realloc(hp->array, newSize * sizeof(struct heapNode));
@@ -260,12 +291,14 @@ int isEmptyHeap(Heap h){
     return (h.front == 1);
 }
 
+/*Function to enqueue a node to the heap*/
 void enqueue(heapNode n, Heap *hp){
     int front = hp->front;
     if(front == hp->size){
         doubleHeapSize(hp);
     }
     hp->array[front] = n;
+    /*Upheap to restore the order*/
     upheap(hp, front);
     hp->front = front + 1;
 }
@@ -275,6 +308,7 @@ void heapEmptyError(){
     abort();
 }
 
+/*Function to create a new graph node given a node id and the weight to it*/
 List newNode(int node, int weight){
     List new = malloc(sizeof(struct ListNode));
     new->weight = weight;
@@ -283,11 +317,14 @@ List newNode(int node, int weight){
     return new;
 }
 
+/*Function to create the list of neighbours for a node, given the matrix of edges
+and the specific node*/
 List newList(int edges[14][3], int node){
     List li = NULL;
     List nodeList = NULL;
     List lastNode = NULL;
     for(int i = 0; i < 14; i++){
+        /*There is an edge incident with the given node*/
         if(edges[i][0] == node || edges[i][1] == node){
             if(edges[i][0] == node){
                 nodeList = newNode(edges[i][1], edges[i][2]);
@@ -295,7 +332,9 @@ List newList(int edges[14][3], int node){
             if(edges[i][1] == node){
                 nodeList = newNode(edges[i][0], edges[i][2]);
             }
+            /*List was empty, add first node*/
             if(lastNode == NULL) li = nodeList;
+            /*There is already a list, append the node to the end*/
             else lastNode->next = nodeList;
             lastNode = nodeList;
         }
@@ -316,10 +355,13 @@ void freeList(List list){
     free(list);
 }
 
+/*Function that given two endpoints of an edge, "deletes" the edge from the
+edges array*/
 void disruptor(int ar[14][3], int city1, int city2) {
     for(int i = 0; i < 14; i++) {
         if((ar[i][0] == city1 && ar[i][1] == city2) ||
         (ar[i][0] == city2 && ar[i][1] == city1)) {
+            /*Set the two endpoints in the edges array to -1*/
             ar[i][0] = -1;
             ar[i][1] = -1;
         }
--- 5trains/graphsmain.c
+++ 5trains/graphsmain.c
@@ -4,7 +4,8 @@
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
-  /*Current edges*/
+  /*Matrix that holds all the given edges, first two columns indicate the
+  incident nodes, third column indicates the weight of the edge*/
   int edges[14][3] = {{1, 2, 51}, {1, 4, 26}, {2, 3, 89}, {3, 5, 63}, {4, 3, 47},
   {5, 6, 111}, {3, 6, 55}, {6, 7, 77}, {4, 7, 51}, {7, 8, 50}, {7, 9, 15}, {9, 10, 40},
   {9, 11, 50}, {10, 11, 34}};
@@ -17,6 +18,7 @@ int main(int argc, char *argv[]) {
   int disruptions = 0;
   scanf("%d\n", &disruptions);
   while(disruptions > 0) {
+          /*Read in disruptions*/
           city1 = readInput();
           city2 = readInput();
           int cityID1 = cityToId(city1);
@@ -27,22 +29,25 @@ int main(int argc, char *argv[]) {
           disruptor(edges, cityID1, cityID2);
   }
   for(int i = 0; i < 11; i++){
+      /*Create the adjacency list for each node*/
       neighbourList[i] = newList(edges, i+1);
   }
   city1 = readInput();
   city2 = readInput();
   while (city1[0] != '!' && city2[0] != '!') {
+    /*Find the shortest path from city1 to city2*/
     dijkstra(neighbourList, cityToId(city1), cityToId(city2));
     free(city1);
     free(city2);
+    /*Read in the next query*/
     city1 = readInput();
     if(city1[0] == '!') break;
     city2 = readInput();
   }
+  /*Free the adjacency lists*/
   for(int i = 0; i < 11; i++){
       freeList(neighbourList[i]);
   }
   free(city1);
-  // free(city2);
   return 0;
 }
